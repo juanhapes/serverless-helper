e0f68716af4db93e8358dd710cae10db1ff9e171
Joaquin Ormaechea <joaquinormaechea@gmail.com>
2019-10-12 16:13:10 -0300

    Bucket helper cors configuration added

--- tests/unit/plugins/core/bucket.js
+++ tests/unit/plugins/core/bucket.js
@@ -57,6 +57,130 @@ describe('Core plugins', () => {
 			});
 		});
 
-	});
+		it('Should return an object with the default CORS configuration if cors prop is true', () => {
 
+			const bucketResult = bucket({
+				resourceName: 'MyBucket',
+				name: 'my-bucket',
+				cors: true
+			});
+
+			assert.deepStrictEqual(bucketResult, {
+				MyBucket: {
+					Type: 'AWS::S3::Bucket',
+					Properties: {
+						AccessControl: 'Private',
+						PublicAccessBlockConfiguration: {
+							BlockPublicAcls: true,
+							BlockPublicPolicy: true,
+							IgnorePublicAcls: true,
+							RestrictPublicBuckets: true
+						},
+						BucketName: 'my-bucket',
+						CorsConfiguration: {
+							CorsRules: [{
+								AllowedMethods: ['*'],
+								AllowedOrigins: ['*']
+							}]
+						}
+					}
+				}
+			});
+		});
+
+		it('Should return an object with custom CORS configuration with one rule', () => {
+
+			const bucketResult = bucket({
+				resourceName: 'MyBucket',
+				name: 'my-bucket',
+				cors: {
+					headers: 'x-foo',
+					exposedHeaders: 'x-bar',
+					id: 'MyRule',
+					maxAge: '60'
+				}
+			});
+
+			assert.deepStrictEqual(bucketResult, {
+				MyBucket: {
+					Type: 'AWS::S3::Bucket',
+					Properties: {
+						AccessControl: 'Private',
+						PublicAccessBlockConfiguration: {
+							BlockPublicAcls: true,
+							BlockPublicPolicy: true,
+							IgnorePublicAcls: true,
+							RestrictPublicBuckets: true
+						},
+						BucketName: 'my-bucket',
+						CorsConfiguration: {
+							CorsRules: [{
+								AllowedMethods: ['*'],
+								AllowedOrigins: ['*'],
+								AllowedHeaders: ['x-foo'],
+								ExposedHeaders: ['x-bar'],
+								Id: 'MyRule',
+								MaxAge: 60
+							}]
+						}
+					}
+				}
+			});
+		});
+
+		it('Should return an object with custom CORS configuration with multiple rules', () => {
+
+			const bucketResult = bucket({
+				resourceName: 'MyBucket',
+				name: 'my-bucket',
+				cors: [{
+					methods: ['GET', 'POST'],
+					origin: ['http://example.com', 'http://www.example.com'],
+					headers: 'x-foo',
+					exposedHeaders: 'x-bar',
+					id: 'MyFirstRule',
+					maxAge: '60'
+				}, {
+					origin: true,
+					headers: ['x-foo', 'x-foo2'],
+					exposedHeaders: ['x-bar', 'x-bar2'],
+					id: 'MySecondRule',
+					maxAge: 300
+				}]
+			});
+
+			assert.deepStrictEqual(bucketResult, {
+				MyBucket: {
+					Type: 'AWS::S3::Bucket',
+					Properties: {
+						AccessControl: 'Private',
+						PublicAccessBlockConfiguration: {
+							BlockPublicAcls: true,
+							BlockPublicPolicy: true,
+							IgnorePublicAcls: true,
+							RestrictPublicBuckets: true
+						},
+						BucketName: 'my-bucket',
+						CorsConfiguration: {
+							CorsRules: [{
+								AllowedMethods: ['GET', 'POST'],
+								AllowedOrigins: ['http://example.com', 'http://www.example.com'],
+								AllowedHeaders: ['x-foo'],
+								ExposedHeaders: ['x-bar'],
+								Id: 'MyFirstRule',
+								MaxAge: 60
+							}, {
+								AllowedMethods: ['*'],
+								AllowedOrigins: ['*'],
+								AllowedHeaders: ['x-foo', 'x-foo2'],
+								ExposedHeaders: ['x-bar', 'x-bar2'],
+								Id: 'MySecondRule',
+								MaxAge: 300
+							}]
+						}
+					}
+				}
+			});
+		});
+	});
 });
