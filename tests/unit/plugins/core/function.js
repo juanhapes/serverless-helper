96252961359a68b469b616ee0e3765f68c092963
Nataniel López <nataniel.lopez@fizzmod.com>
2020-03-04 18:00:33 -0300

    Added events for functions and moved schedule property to events

--- tests/unit/plugins/core/function.js
+++ tests/unit/plugins/core/function.js
@@ -30,8 +30,18 @@ describe('Core plugins', () => {
 				functionName: 'MyFunction',
 				handler: 'path/to/handler.export',
 				description: 'My super description',
-				schedule: 'rate(1 hour)',
 				timeout: 6,
+				events: [
+					{
+						schedule: 'rate(1 hour)'
+					},
+					{
+						s3: {
+							bucket: 'myBucket',
+							event: 's3:ObjectCreated:*'
+						}
+					}
+				],
 				package: { include: ['path/to/includ/file.js'] }
 			});
 
@@ -40,7 +50,40 @@ describe('Core plugins', () => {
 					MyFunction: {
 						handler: 'path/to/handler.export',
 						description: 'My super description',
-						schedule: 'rate(1 hour)',
+						timeout: 6,
+						events: [
+							{
+								schedule: 'rate(1 hour)'
+							},
+							{
+								s3: {
+									bucket: 'myBucket',
+									event: 's3:ObjectCreated:*'
+								}
+							}
+						],
+						package: { include: ['path/to/includ/file.js'] }
+					}
+				}]
+			});
+		});
+
+		it('Should not include the events property when it isn\'t an array', () => {
+
+			const lambdaFunctionResult = lambdaFunction({}, {
+				functionName: 'MyFunction',
+				handler: 'path/to/handler.export',
+				description: 'My super description',
+				timeout: 6,
+				events: { not: 'an array' },
+				package: { include: ['path/to/includ/file.js'] }
+			});
+
+			assert.deepStrictEqual(lambdaFunctionResult, {
+				functions: [{
+					MyFunction: {
+						handler: 'path/to/handler.export',
+						description: 'My super description',
 						timeout: 6,
 						package: { include: ['path/to/includ/file.js'] }
 					}
