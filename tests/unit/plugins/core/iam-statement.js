0a655233698e54ab10db614d0f46f63bb382f8f7
Joaquín Ormaechea <joaquinormaechea@gmail.com>
2020-04-03 20:01:55 -0300

    Custom role support added for iamStatement hook

--- tests/unit/plugins/core/iam-statement.js
+++ tests/unit/plugins/core/iam-statement.js
@@ -167,5 +167,393 @@ describe('Core plugins', () => {
 				}
 			});
 		});
+
+		context('When a custom role is defined', () => {
+
+			it('Should throw if custom role is not a string', () => {
+				assert.throws(() => iamStatement({
+					provider: {
+						role: ['ServiceExecutionRole']
+					}
+				}, {
+					action: 's3:putObject',
+					resource: 'my-bucket-resource'
+				}));
+			});
+
+			it('Should throw if custom role is an external ARN', () => {
+				assert.throws(() => iamStatement({
+					provider: {
+						role: 'aws:arn:xxxxxxx:some:resource'
+					}
+				}, {
+					action: 's3:putObject',
+					resource: 'my-bucket-resource'
+				}));
+			});
+
+			it('Should throw if resources property is not defined', () => {
+				assert.throws(() => iamStatement({
+					provider: {
+						role: 'ServiceExecutionRole'
+					}
+				}, {
+					action: 's3:putObject',
+					resource: 'my-bucket-resource'
+				}));
+			});
+
+			it('Should throw if resources.Resources property is not defined', () => {
+				assert.throws(() => iamStatement({
+					provider: {
+						role: 'ServiceExecutionRole'
+					},
+					resources: {}
+				}, {
+					action: 's3:putObject',
+					resource: 'my-bucket-resource'
+				}));
+			});
+
+			it('Should throw if the role is not in the Resources (as object)', () => {
+				assert.throws(() => iamStatement({
+					provider: {
+						role: 'ServiceExecutionRole'
+					},
+					resources: {
+						Resources: {
+							MyBucket: {
+								Type: 'AWS::Bucket'
+							}
+						}
+					}
+				}, {
+					action: 's3:putObject',
+					resource: 'my-bucket-resource'
+				}));
+			});
+
+			it('Should throw if the role is not in the Resources (as array of objects)', () => {
+				assert.throws(() => iamStatement({
+					provider: {
+						role: 'ServiceExecutionRole'
+					},
+					resources: {
+						Resources: [
+							{
+								MyBucket: {
+									Type: 'AWS::Bucket'
+								}
+							}
+						]
+					}
+				}, {
+					action: 's3:putObject',
+					resource: 'my-bucket-resource'
+				}));
+			});
+
+			it('Should create and update the policy on consecutive calls if the role resource is found in the Resources (as object)', () => {
+
+				const result = iamStatement(iamStatement({
+					provider: {
+						role: 'ServiceExecutionRole'
+					},
+					resources: {
+						Resources: {
+							ServiceExecutionRole: {
+								Type: 'AWS::IAM::Role',
+								Properties: {
+									RoleName: 'MyCustomService-lambdaRole',
+									Path: '/',
+									AssumeRolePolicyDocument: {
+										Version: '2012-10-17',
+										Statement: [
+											{
+												Effect: 'Allow',
+												Principal: {
+													Service: [
+														'lambda.amazonaws.com'
+													]
+												},
+												Action: 'sts:AssumeRole'
+											}
+										]
+									},
+									Policies: [
+										{
+											PolicyName: 'some-other-policy',
+											PolicyDocument: {
+												Version: '2012-10-17',
+												Statement: [
+													{
+														Effect: 'Allow',
+														Action: [
+															'logs:CreateLogGroup',
+															'logs:CreateLogStream',
+															'logs:PutLogEvents'
+														],
+														Resource: [
+															{
+																'Fn::Join': [
+																	':',
+																	[
+																		'arn:aws:logs',
+																		{ Ref: 'AWS::Region' },
+																		{ Ref: 'AWS::AccountId' },
+																		'log-group:/aws/lambda/*:*'
+																	]
+																]
+															}
+														]
+													}
+												]
+											}
+										}
+									]
+								}
+							}
+						}
+					}
+				}, {
+					action: 's3:putObject',
+					resource: 'my-bucket-resource'
+				}), {
+					action: 's3:getObject',
+					resource: 'my-bucket-resource'
+				});
+
+				assert.deepStrictEqual(result, {
+					provider: {
+						role: 'ServiceExecutionRole'
+					},
+					resources: {
+						Resources: {
+							ServiceExecutionRole: {
+								Type: 'AWS::IAM::Role',
+								Properties: {
+									RoleName: 'MyCustomService-lambdaRole',
+									Path: '/',
+									AssumeRolePolicyDocument: {
+										Version: '2012-10-17',
+										Statement: [
+											{
+												Effect: 'Allow',
+												Principal: {
+													Service: [
+														'lambda.amazonaws.com'
+													]
+												},
+												Action: 'sts:AssumeRole'
+											}
+										]
+									},
+									Policies: [
+										{
+											PolicyName: 'some-other-policy',
+											PolicyDocument: {
+												Version: '2012-10-17',
+												Statement: [
+													{
+														Effect: 'Allow',
+														Action: [
+															'logs:CreateLogGroup',
+															'logs:CreateLogStream',
+															'logs:PutLogEvents'
+														],
+														Resource: [
+															{
+																'Fn::Join': [
+																	':',
+																	[
+																		'arn:aws:logs',
+																		{ Ref: 'AWS::Region' },
+																		{ Ref: 'AWS::AccountId' },
+																		'log-group:/aws/lambda/*:*'
+																	]
+																]
+															}
+														]
+													}
+												]
+											}
+										},
+										{
+											PolicyName: 'sls-helper-custom-policy',
+											PolicyDocument: {
+												Version: '2012-10-17',
+												Statement: [
+													{
+														Effect: 'Allow',
+														Action: 's3:putObject',
+														Resource: 'my-bucket-resource'
+													},
+													{
+														Effect: 'Allow',
+														Action: 's3:getObject',
+														Resource: 'my-bucket-resource'
+													}
+												]
+											}
+										}
+									]
+								}
+							}
+						}
+					}
+				});
+			});
+
+			it('Should create and update the policy on consecutive calls if the role resource is found in the Resources (as array of object)', () => {
+
+				const result = iamStatement(iamStatement({
+					provider: {
+						role: 'ServiceExecutionRole'
+					},
+					resources: {
+						Resources: [{
+							ServiceExecutionRole: {
+								Type: 'AWS::IAM::Role',
+								Properties: {
+									RoleName: 'MyCustomService-lambdaRole',
+									Path: '/',
+									AssumeRolePolicyDocument: {
+										Version: '2012-10-17',
+										Statement: [
+											{
+												Effect: 'Allow',
+												Principal: {
+													Service: [
+														'lambda.amazonaws.com'
+													]
+												},
+												Action: 'sts:AssumeRole'
+											}
+										]
+									},
+									Policies: [
+										{
+											PolicyName: 'some-other-policy',
+											PolicyDocument: {
+												Version: '2012-10-17',
+												Statement: [
+													{
+														Effect: 'Allow',
+														Action: [
+															'logs:CreateLogGroup',
+															'logs:CreateLogStream',
+															'logs:PutLogEvents'
+														],
+														Resource: [
+															{
+																'Fn::Join': [
+																	':',
+																	[
+																		'arn:aws:logs',
+																		{ Ref: 'AWS::Region' },
+																		{ Ref: 'AWS::AccountId' },
+																		'log-group:/aws/lambda/*:*'
+																	]
+																]
+															}
+														]
+													}
+												]
+											}
+										}
+									]
+								}
+							}
+						}]
+					}
+				}, {
+					action: 's3:putObject',
+					resource: 'my-bucket-resource'
+				}), {
+					action: 's3:getObject',
+					resource: 'my-bucket-resource'
+				});
+
+				assert.deepStrictEqual(result, {
+					provider: {
+						role: 'ServiceExecutionRole'
+					},
+					resources: {
+						Resources: [{
+							ServiceExecutionRole: {
+								Type: 'AWS::IAM::Role',
+								Properties: {
+									RoleName: 'MyCustomService-lambdaRole',
+									Path: '/',
+									AssumeRolePolicyDocument: {
+										Version: '2012-10-17',
+										Statement: [
+											{
+												Effect: 'Allow',
+												Principal: {
+													Service: [
+														'lambda.amazonaws.com'
+													]
+												},
+												Action: 'sts:AssumeRole'
+											}
+										]
+									},
+									Policies: [
+										{
+											PolicyName: 'some-other-policy',
+											PolicyDocument: {
+												Version: '2012-10-17',
+												Statement: [
+													{
+														Effect: 'Allow',
+														Action: [
+															'logs:CreateLogGroup',
+															'logs:CreateLogStream',
+															'logs:PutLogEvents'
+														],
+														Resource: [
+															{
+																'Fn::Join': [
+																	':',
+																	[
+																		'arn:aws:logs',
+																		{ Ref: 'AWS::Region' },
+																		{ Ref: 'AWS::AccountId' },
+																		'log-group:/aws/lambda/*:*'
+																	]
+																]
+															}
+														]
+													}
+												]
+											}
+										},
+										{
+											PolicyName: 'sls-helper-custom-policy',
+											PolicyDocument: {
+												Version: '2012-10-17',
+												Statement: [
+													{
+														Effect: 'Allow',
+														Action: 's3:putObject',
+														Resource: 'my-bucket-resource'
+													},
+													{
+														Effect: 'Allow',
+														Action: 's3:getObject',
+														Resource: 'my-bucket-resource'
+													}
+												]
+											}
+										}
+									]
+								}
+							}
+						}]
+					}
+				});
+			});
+		});
 	});
 });
